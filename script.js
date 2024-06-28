bb616541f95ae8a0654c927bab5b25e05aca8028
Svisuals <feraso.svisuals@gmail.com>
2024-06-27 23:26:01 -0300

    update08

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let syncScroll = true;
+// Inicializando a biblioteca iframe-resizer
+iFrameResize({ log: true }, '#iframe1', '#iframe2');
 
+let syncScroll = true;
 const iframe1 = document.getElementById('iframe1');
 const iframe2 = document.getElementById('iframe2');
 const syncToggle = document.getElementById('syncToggle');
@@ -8,33 +10,21 @@ const lockIcon = document.getElementById('lockIcon');
 function syncScrolling(event) {
     if (!syncScroll) return;
 
-    const sourceIframe = event.target;
-    const otherIframe = sourceIframe === iframe1.contentWindow ? iframe2.contentWindow : iframe1.contentWindow;
+    const otherIframe = event.target === iframe1 ? iframe2 : iframe1;
+    const targetIframe = event.target.contentWindow;
 
-    otherIframe.scrollTo(sourceIframe.scrollX, sourceIframe.scrollY);
+    otherIframe.contentWindow.scrollTo(targetIframe.scrollX, targetIframe.scrollY);
 }
 
-function addScrollSync(iframe) {
-    $(iframe.contentWindow).on('scroll', function() {
-        if (syncScroll) {
-            const scrollTop = $(this).scrollTop();
-            const scrollLeft = $(this).scrollLeft();
-            if (iframe === iframe1) {
-                $(iframe2.contentWindow).scrollTop(scrollTop);
-                $(iframe2.contentWindow).scrollLeft(scrollLeft);
-            } else {
-                $(iframe1.contentWindow).scrollTop(scrollTop);
-                $(iframe1.contentWindow).scrollLeft(scrollLeft);
-            }
-        }
-    });
-}
+iframe1.addEventListener('load', () => {
+    iframe1.contentWindow.addEventListener('scroll', syncScrolling);
+});
 
-iframe1.addEventListener('load', () => addScrollSync(iframe1));
-iframe2.addEventListener('load', () => addScrollSync(iframe2));
+iframe2.addEventListener('load', () => {
+    iframe2.contentWindow.addEventListener('scroll', syncScrolling);
+});
 
 syncToggle.addEventListener('click', () => {
     syncScroll = !syncScroll;
     lockIcon.src = syncScroll ? 'locked.png' : 'unlocked.png';
-    lockIcon.alt = syncScroll ? 'Cadeado Fechado' : 'Cadeado Aberto';
 });
